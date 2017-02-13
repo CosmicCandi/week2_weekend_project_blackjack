ffc396d4a57edc2107d01e5e836f1b13bd417154
CosmicCandi <kalea.wolff@gmail.com>
2017-02-12 23:37:29 -0500

    Adventurer mode complete. Tested and working properly.

--- blackjack.rb
+++ blackjack.rb
@@ -32,6 +32,13 @@ class Blackjack
     2.times do
       @p1_hand << deck.draw_a_card
     end
+    #Used to test winner condition 1 on line 109 below
+    # @p1_hand << Card.new("Diamonds", 2)
+    # @p1_hand << Card.new("Hearts", 2)
+    # @p1_hand << Card.new("Clubs", 2)
+    # @p1_hand << Card.new("Spades", 2)
+    # @p1_hand << Card.new("Hearts", 3)
+    # @p1_hand << Card.new("Spades", 3)
     2.times do
       @dealer_hand << deck.draw_a_card
     end
@@ -43,11 +50,11 @@ class Blackjack
 
   def play
     show_player_hand
-    if total_hands(p1_hand) == 21
+    if total_hands(p1_hand) == 21 || total_hands(dealer_hand) == 21
       winner
     elsif total_hands(p1_hand) > 21
       @busted = true
-      winner
+      loser
     else
       show_dealer_last
       player_turn
@@ -99,14 +106,18 @@ class Blackjack
 
   def winner
     catchphrase = "You win with #{total_hands(p1_hand)}!"
-    if total_hands(p1_hand) > total_hands(dealer_hand)
+    if total_hands(p1_hand) <= 21 && p1_hand.length == 6
+      puts "We've just witnessed a miracle! #{p1_hand.length} cards and no bust! I can't believe you ended with a total of #{total_hands(p1_hand)}!"
+      puts " (╯°□°）╯︵ ┻━┻ YOU WINNNNNN!"
+      new_game?
+    elsif total_hands(p1_hand) > total_hands(dealer_hand)
       puts catchphrase
       new_game?
     elsif total_hands(p1_hand) == 21
       puts "BLACKJACK! #{catchphrase}"
       new_game?
     elsif total_hands(p1_hand) == total_hands(dealer_hand)
-      puts "Ties go to the player! #{catchphrase}"
+      deal_with_ties
       new_game?
     elsif total_hands(dealer_hand) > total_hands(p1_hand)
         if total_hands(dealer_hand) > 21
@@ -126,6 +137,16 @@ class Blackjack
     elsif total_hands(dealer_hand) > 21
       puts "The dealer busted with #{total_hands(dealer_hand)}! #{catchphrase}"
       new_game?
+    end
+  end
+
+  def deal_with_ties
+    if p1_hand.length > dealer_hand.length
+      puts "You and the dealer tied, but you won with #{p1_hand.length} cards and a hand total of #{total_hands(p1_hand)}."
+    elsif dealer_hand.length > p1_hand.length
+      puts "You and the dealer tied, but the dealer won with #{dealer_hand.length} cards and a hand total of #{total_hands(dealer_hand)}."
+    else
+      puts "You and the dealer tied, but hand totals were also tied! You won with #{p1_hand.length} cards and a hand total of #{total_hands(p1_hand)}"
     end
   end
 
@@ -157,20 +178,6 @@ class Blackjack
 end
 Blackjack.new.play
 
-# binding.pry
-
-# puts "Would you like to (H)it or (S)tand?"
-#   response = gets.chomp.downcase
-# if response == "h"
-#   @p1_hand << game.p1_hand.deck.draw_a_card
-#   @hand_total += game.p1_hand.last.value
-#   puts "You have #{game.p1_hand.join(" and ")}"
-#   puts "Would you like to (H)it or (S)tand?"
-# else
-#   puts "You (won) || (lost)"
-#     exit
-# end
-
 ### Explorer Mode ###
 # Don't consider Aces as possible 1's ... they are always 11s
 # (this means you can bust on the deal)
@@ -182,20 +189,20 @@ Blackjack.new.play
 # New deck every game
 # deck must be shuffled every game
 # no betting at all
-# must have suits (ace of diamonds)
-# Dealer hits if less than 16, otherwise dealer stays
-# You enter what you play (hit or stay)
+# must have suits (ace of diamonds) = Working
+# Dealer hits if less than 16, otherwise dealer stays = Working
+# You enter what you play (hit or stay) = Working
 # No if you get 5 cards you win funnybusiness
-# get as close to 21 without going over
-# Must beat the dealer
-# you can see 1 of dealers cards, while you are playing and both when your hand is over.
-# If you get blackjack, you win automagically
-# Ties go to the player
+# get as close to 21 without going over = Working
+# Must beat the dealer = Working
+# you can see 1 of dealers cards, while you are playing and both when your hand is over. = Working
+# If you get blackjack, you win automagically = Working
+# Ties go to the player = Working
 
 ### Adventure Mode ###
 # You win if you have 6 cards and stay under 21
-# Dealer wins if it draws blackjack before any player actions
-# Ties go to the hand with the most cards, if still tied, it goes to the player
+# Dealer wins if it draws blackjack before any player actions  = Tested and working
+# Ties go to the hand with the most cards, if still tied, it goes to the player - Tested and working
 
 ### Epic Mode ###
 # Add the idea of tracking your progress as you play over time.
